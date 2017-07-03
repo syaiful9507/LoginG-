4bba8e1cc8cb8b15717bf534aabeba098674bbc1
syaiful9507 <syaiful9507@gmail.com>
2017-07-03 13:50:32 +0700

    sen, jul 3 13:50

---
+++ app/src/main/java/syaiful/learn/logingoogle/util/Constants.java
@@ -0,0 +1,12 @@
+package syaiful.learn.logingoogle.util;
+
+/**
+ * Created by syaiful9508 on 16/06/17.
+ */
+
+public class Constants {
+    public static final String FIREBASE_URL = "https:YOUR FIREBASE URL";
+    public static final String FIREBASE_LOCATION_USERS = "users";
+    public static final String FIREBASE_URL_USERS = FIREBASE_URL + "/" + FIREBASE_LOCATION_USERS;
+    public static final String FIREBASE_PROPERTY_TIMESTAMP = "timestamp";
+}
